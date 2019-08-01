1f986d804a989d94ddeee74dc004a9726dd1beda
Kishan Shaw <kishanshaw29@gmail.com>
2019-08-01 09:47:36 +0530

    8th commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to the Home page!
+          This is the 8th commit!
         </p>
         <a
           className="App-link"
